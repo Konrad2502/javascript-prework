3a4b75062eebccd06b9291974aadec6840b297be
Konrad Litak <konrad.litak@gmail.com>
2023-02-15 18:37:22 +0000

    Add playerInput

--- javascript-prework/js/script.js
+++ javascript-prework/js/script.js
@@ -28,3 +28,17 @@ if(randomNumber == 1) {
     computerMove ='papier';
 }
 printMessage('Moj ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if(playerInput == '2') {
+    playerMove = 'nozyczki';
+} else if(playerInput == '3') {
+    playerMove = 'papier'
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
