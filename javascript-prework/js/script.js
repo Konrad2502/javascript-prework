2335848ce0cbba15d96b5ccc912072924c18340b
Konrad Litak <konrad.litak@gmail.com>
2023-02-15 18:26:09 +0000

    Add else if

--- javascript-prework/js/script.js
+++ javascript-prework/js/script.js
@@ -12,3 +12,18 @@ function clearMessages(){
 var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+
+
+var randomNumber = Math.floor(Math.random() * 9) + 11;
+// printMessage('Wylosowana liczba to: ' + randomNumber);
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+printMessage('wylosowana liczba to: ' + randomNumber);
+if(randomNumber == 1) {
+    computerMove = 'kamien';
+} else if(randomNumber == 2) {
+    computerMove = 'nozyczki';
+} else {
+    computerMove ='papier';
+}
